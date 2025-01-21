98d1250fd0fb21d3331c819b5bda4cc6827877e6
Anxhelo Pjetrani <AnxPje18@htl-shkoder.com>
2025-01-21 10:23:01 +0100

    Fixing inshallah

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -3,11 +3,12 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
     private Calculator calculator;
+
 
     @BeforeEach
     void setUp() {
@@ -16,27 +17,40 @@ class CalculatorTest {
 
     @Test
     void add() {
-        assertEquals(7, calculator.add(3, 4));
-        assertNotEquals(9, calculator.add(5, 5));
+        assertEquals(8, calculator.add(3,5));
+        assertNotEquals(8, calculator.add(3,4));
     }
 
     @Test
     void subtract() {
-        assertEquals(1,calculator.subtract(4,3));
-        assertNotEquals(9,calculator.subtract(5,5));
+        assertEquals(-8, calculator.subtract(10, 18));
+        assertNotEquals(10, calculator.subtract(2,3) );
+    }
+
+    @Test
+    void divide()  {
+        try {
+            assertEquals(5, calculator.divide(20, 4));
+        } catch (DivideByZeroException e) {
+            throw new RuntimeException();
+        }
+
+        try {
+            assertNotEquals(3, calculator.divide(4,1));
+        } catch (DivideByZeroException e) {
+            throw new RuntimeException();
+        }
+
+        assertThrows(DivideByZeroException.class, () -> calculator.divide(12, 0));
     }
 
     @Test
     void multiply() {
-        assertEquals(9,calculator.multiply(3,3));
-        assertNotEquals(9,calculator.multiply(5,5));
+        assertEquals(12, calculator.multiply(3,4));
+        assertNotEquals(12, calculator.multiply(3,3));
     }
 
-    @Test
-    void divide() {
-        assertEquals(7,calculator.divide(28,4));
-        assertNotEquals(9,calculator.divide(5,5));
-        assertThrows(IllegalArgumentException.class, () -> calculator.divide(12, 0));
-    }
+
+
 
 }
